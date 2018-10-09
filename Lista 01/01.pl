3f30dd391dbe5b00dde22442de2eee022c5f4da3
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-10-09 08:53:28 -0300

    Questão 01

---
+++ Lista 01/01.pl
@@ -0,0 +1,20 @@
+passaro(joao).
+peixe(pedro).
+minhoca(maria).
+gato(chuckNorris).
+amigo(gato).
+
+gostam(passaro, minhoca).
+gostam(joao, maria).
+gostam(gato, joao).
+gostam(gato, peixe).
+gostam(amigo, amigo).
+
+come(gato, peixe).
+come(gato, pedro).
+come(gato, minhoca).
+come(gato, maria).
+come(gato, joao).
+come(gato, passaro).
+
+
