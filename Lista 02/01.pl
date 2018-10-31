35148130385e7298cd3c42b28f327bcb48c2aaf8
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-10-31 03:27:15 -0300

    Questão 01

---
+++ Lista 02/01.pl
@@ -0,0 +1,43 @@
+homem(joao).
+homem(jose).
+homem(mario).
+homem(carlos).
+mulher(ana).
+mulher(helena).
+mulher(joana).
+mulher(maria).
+
+progenitor(jose, joao).
+progenitor(jose, ana).
+progenitor(maria, joao).
+progenitor(maria, ana).
+progenitor(ana, helena).
+progenitor(ana, joana).
+progenitor(joao, mario).
+progenitor(helena, carlos).
+progenitor(mario, carlos).
+
+
+filho(X, Y):- homem(X), progenitor(Y, X).
+filha(X, Y):- mulher(X), progenitor(Y, X).
+
+pai(X, Y):- homem(X), progenitor(X, Y).
+mae(X, Y):- mulher(x), progenitor(X, Y).
+
+irmão(X, Y):- homem(X), progenitor(Z, Y), progenitor(Z, X).
+irmã(X, Y):- mulher(X), progenitor(Z, Y), progenitor(Z, X).
+
+avô(X, Y):- homem(X), ( filho(Z, X); filha(Z, X) ), progenitor(Z, Y).
+avó(X, Y):- mulher(X), (  filho(Z, X); filha(Z, X) ), progenitor(Z, Y).
+
+tio(X, Y):- homem(X), imão(Z, X), progenitor(Z, Y).
+tia(X, Y):- mulher(X), irmã(Z, X), progenitor(Z, Y).
+
+primo(X, Y):- homem(X), tio(Z, X), progenitor(Z, Y).
+
+
+
+
+
+
+
