3ec70a9e812ade0b11069f8aefd5dcf2d2f3b76e
Mateus Henrique <mateushenriquechaves1@gmail.com>
2018-11-10 20:15:49 -0300

    Objetos e Atributos

---
+++ Exercícios da internet/Objetos e Atributos.pl
@@ -0,0 +1,33 @@
+objecto(obj1).
+objecto(obj2).
+objecto(obj3).
+objecto(obj4).
+objecto(obj5).
+
+material(obj1,madeira).
+material(obj2,madeira).
+material(obj3, vidro).
+material(obj4, vidro).
+material(obj5, ferro).
+
+tipo(obj1, mesa).
+tipo(obj2, cadeira).
+tipo(obj3, mesa).
+tipo(obj4, jarra).
+tipo(obj5, matraca).
+
+peso(obj1, 10.5).
+peso(obj2, 1.5).
+peso(obj3, 1.6).
+peso(obj4, 0.5).
+peso(obj5, 1.8). 
+
+fragil(vidro).
+forte(ferro).
+
+sobre(obj4, obj1).
+sobre(obj3, obj1).
+sobre(obj5, obj3).
+
+% A - Escreva o predicado debaixo(O1,O2) que sucede se o objecto O1 está debaixo (directamente) do objecto O2. 
+debaixo(X, Y):- sobre(Y, X).
